363a040bd68760af9b5c4fbb7a4371bb48b2cd1c
lemon927_ <45076735+fpahs927@users.noreply.github.com>
2024-01-29 13:05:44 +0900

    ?
    이건뭐야

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,9 +6,11 @@ import com.example.demo.entity.Item;
 import com.example.demo.service.ItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -18,29 +20,43 @@ import java.util.List;
 public class ItemController {
     @Autowired
     private ItemService itemService;
-    @GetMapping("/itemlist")   //모든 회원 리스트 뿌리기
-    public List<Item> GetAllMember(){
-        return itemService.getAllMembers();
-    }
-    @PostMapping("/addItem") //db에 추가하기
-    public ResponseDTO createForm (@RequestBody Item item) {
-        item.setItemName(item.getItemName());
-        item.setPrice(item.getPrice());
-        itemService.saveItem(item);
-        return ResponseDTO.test("작업 중"); //책 목록
-    }
 
-    @GetMapping("/items")
-    public ResponseDTO list(Model itemsform){
-        List<Item> items = itemService.findItems();   //service에서 가져 온 다음에
-        itemsform.addAttribute("items", items);    //items에 다 뿌려
+    @GetMapping("/items/new")
+    public ResponseDTO viewPersonInfo(@RequestParam("item_id") int id){
+        Item item = itemService.findItems(id);
+        ItemDto itemDto = new ItemDto();
+        itemDto.setPrice(item.getPrice()); // <--이런식으로 하면 프런트한테 값을 전달해준다
+        System.out.println("id값은->" + item.getId() + "이름은" + item.getItemName()
+            +"가격은" + item.getPrice() + "입니다");
+        return ResponseDTO.of();
+    }
+    @GetMapping("/allItem")
+    public ResponseDTO AllInfo(){
+        List<Item> items = itemService.getAllItems();
+        System.out.print("테스트해보자" + items);
         return ResponseDTO.test("작업 중");
     }
-    @GetMapping("/items/{item_id}/edit")  //특정 아이디 값으로 해당 id 값 name 지우기
+    @GetMapping("/items/{item_id}/edit")
     public ResponseDTO updateItem(@RequestParam("item_id") int id,
                                   @RequestParam("itemName") String name){
         itemService.updateItemName(id, name);
         return ResponseDTO.test("작업 중");
 
     }
+    @PostMapping("/addItem") //db에 추가하기
+    public ResponseDTO createForm (@RequestBody ItemDto item) {
+        item.setItemName(item.getItemName());
+        item.setPrice(item.getPrice());
+        item.setStockQuantity(item.getStockQuantity());
+        itemService.saveItem((Item) item);
+        return ResponseDTO.test("작업 중"); //책 목록
+    }
+    ///////////페이징 처리를 해보고싶었다
+    @GetMapping("/list")
+    @ResponseBody
+    public List<Item> GetItemList(@PageableDefault(page =0, size=3, sort="id") Pageable pageable){
+        //page 0부터 시작할 때 3개의 값만 나오고, 정렬은 id로 한다
+        return itemService.FindBooksBypageRequest(pageable);
+    }
+
 }
