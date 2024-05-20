adfdccfcfa636efcb3e15e52e20aca0c0d64d32c
lemon927_ <45076735+fpahs927@users.noreply.github.com>
2024-05-20 23:43:36 +0900

    디자인은 얼추 어떻게 하는지 알겠고 기능 구현 ㄱㄱ

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -16,70 +16,47 @@ import java.util.List;
 
 @RequestMapping("/student")
 @Controller
-
 public class StudentController {
     private KakaoTokenJsonData kakaoTokenJsonData;
     private KakaoUserInfo kakaoUserInfo;
 
     @Autowired
     private StudentService studentService;
-    @GetMapping("/list")   //모든 회원 리스트 뿌리기
-    public List<Student> GetAllMember(){
+
+    @GetMapping("/list")   // 모든 회원 리스트 뿌리기
+    public List<Student> getAllMembers() {
         return studentService.getAllMembers();
     }
 
     @PostMapping("/createStudent")
-    public ResponseDTO createStudent(@RequestBody StudentDTO student) {
+    public String createStudent(@RequestBody StudentDTO student) {
         studentService.join(student);
-        return ResponseDTO.create(true,"회원가입성공").toResponseDTO();
+        return "joinForm";
     }
 
-//    @GetMapping("/loginTakao")
-//    public String loginForm(){
-//        return "loginTakao";
-//    }
-
-//    @Description("회원이 소셜 로그인을 마치면 자동으로 실행되는 API입니다. 인가 코드를 이용해 토큰을 받고, 해당 토큰으로 사용자 정보를 조회합니다." +
-//            "사용자 정보를 이용하여 서비스에 회원가입합니다.")
-//    @GetMapping("/oauth")
-//    @ResponseBody
-//    public String kakaoOauth(@RequestParam("code") String code) {
-//        log.info("인가 코드를 이용하여 토큰을 받습니다.");
-//        KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
-//        log.info("토큰에 대한 정보입니다.{}",kakaoTokenResponse);
-//        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token());
-//        log.info("회원 정보 입니다.{}",userInfo);
-//
-//        studentService.join2(userInfo.getKakao_account().getEmail());
-//
-//        return "okay";
-//    }
-
-
-    @GetMapping("/login")
+    @GetMapping("/loginPage")
     public String loginForm() {
-        System.out.println("test");
-        return "login";
+        return "login"; // login.html 템플릿 반환
     }
 
-//    @PostMapping("/login")
-//    public ResponseDTO loginStudent(@RequestBody StudentLoginDTO login) {
-//        long defaultLogin = studentService.login(login.getEmail(), login.getPassword());
-//        if (defaultLogin == -1){
-//            return ResponseDTO.create(false,"이메일틀림").toResponseDTO();
-//
-//        }
-//        if(defaultLogin == -2){
-//            return ResponseDTO.create(false,"비밀번호틀림").toResponseDTO();
-//
-//        }
-//
-//        return ResponseDTO.create(true,"로그인성공").toResponseDTO();
-//    }
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password, Model model) {
+        long loginResult = studentService.login(email, password);
+        if (loginResult == -1) {
+            model.addAttribute("error", "이메일이 틀렸습니다.");
+            return "login"; // 로그인 페이지로 다시 돌아감
+        }
+        if (loginResult == -2) {
+            model.addAttribute("error", "비밀번호가 틀렸습니다.");
+            return "login"; // 로그인 페이지로 다시 돌아감
+        }
+        return "redirect:/home"; // 로그인 성공 시 홈 페이지로 리다이렉트
+    }
+
     @GetMapping("/myinfo")
-    public ResponseDTO gradeStudent(@RequestParam Long studentId){
+    public ResponseDTO gradeStudent(@RequestParam Long studentId) {
         studentService.myInfo(studentId);
         return ResponseDTO.test("작업중입니다.");
     }
-
 }
+
